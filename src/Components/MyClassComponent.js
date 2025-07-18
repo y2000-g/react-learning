bc0218b72a737cd2f5090e580a11894cc438fd3f
Yashita Gole <yashitagole2019@gmail.com>
2025-07-18 15:16:47 +0530

    Practiced Basic react like function, promise, class and functional component, knows jsx and Dom manipulation and practicing component life cycle

--- src/Components/MyClassComponent.js
+++ src/Components/MyClassComponent.js
@@ -22,6 +22,15 @@ class MyClassComponent extends React.Component
         return state1
     }
 
+    shouldComponentUpdate()
+    {
+       console.log("-==Life Cycle==- shouldComponentUpdate")  
+       if(this.state1.counter == 5)
+            return false
+        else
+            return true
+    }
+
     render()
     {
         console.log("-==Life Cycle==- render")
@@ -34,6 +43,19 @@ class MyClassComponent extends React.Component
             )
     }
 
+    getSnapshotBeforeUpdate(prevProps, prevState)
+    {
+        console.log("-==Life Cycle==- componentDidMount")
+        console.log("Current props : ", this.props)
+        console.log("Current Props : ", prevProps)
+        console.log("Current State : ", this.state1)
+        console.log("Current State : ", prevState)
+    }
+
+    componentDidUpdate()
+    {
+        console.log("-==Life Cycle==- componentDidUpdate")
+    }
     componentDidMount()
     {
         console.log("-==Life Cycle==- componentDidMount")
